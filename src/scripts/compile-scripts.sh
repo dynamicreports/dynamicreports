313789a6340ea59dd358ab677ec7791764f1d8b2
Edwin Njeru <mailnjeru@gmail.com>
2018-11-21 08:48:00 +0300

    :fire: Remove the scripts profile

--- src/scripts/compile-scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
-
-sudo apt-get install ttf-mscorefonts-installer
