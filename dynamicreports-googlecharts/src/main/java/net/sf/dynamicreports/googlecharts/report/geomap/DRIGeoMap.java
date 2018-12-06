15ed8c9ef94347bdbdfba7d80ab511b2826ee4ba
Edwin Njeru <mailnjeru@gmail.com>
2018-12-06 09:51:16 +0300

    :fire: Remove redundant access modifiers in the charts module

--- dynamicreports-googlecharts/src/main/java/net/sf/dynamicreports/googlecharts/report/geomap/DRIGeoMap.java
+++ dynamicreports-googlecharts/src/main/java/net/sf/dynamicreports/googlecharts/report/geomap/DRIGeoMap.java
@@ -7,16 +7,16 @@
  * This file is part of DynamicReports.
  *
  * DynamicReports is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU Lesser General License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * DynamicReports is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * GNU Lesser General License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU Lesser General License
  * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
  */
 package net.sf.dynamicreports.googlecharts.report.geomap;
@@ -40,40 +40,40 @@ public interface DRIGeoMap extends DRICustomComponent {
      *
      * @return a {@link java.lang.Boolean} object.
      */
-    public Boolean getShowLegend();
+    Boolean getShowLegend();
 
     /**
      * <p>getDataMode.</p>
      *
      * @return a {@link net.sf.dynamicreports.googlecharts.report.geomap.GeoMapDataMode} object.
      */
-    public GeoMapDataMode getDataMode();
+    GeoMapDataMode getDataMode();
 
     /**
      * <p>getRegionExpression.</p>
      *
      * @return a {@link net.sf.dynamicreports.report.definition.expression.DRIExpression} object.
      */
-    public DRIExpression<String> getRegionExpression();
+    DRIExpression<String> getRegionExpression();
 
     /**
      * <p>getValueLabelExpression.</p>
      *
      * @return a {@link net.sf.dynamicreports.report.definition.expression.DRIExpression} object.
      */
-    public DRIExpression<String> getValueLabelExpression();
+    DRIExpression<String> getValueLabelExpression();
 
     /**
      * <p>getColors.</p>
      *
      * @return a {@link java.util.List} object.
      */
-    public List<Color> getColors();
+    List<Color> getColors();
 
     /**
      * <p>getDataset.</p>
      *
      * @return a {@link net.sf.dynamicreports.googlecharts.report.geomap.DRIGeoMapDataset} object.
      */
-    public DRIGeoMapDataset getDataset();
+    DRIGeoMapDataset getDataset();
 }
